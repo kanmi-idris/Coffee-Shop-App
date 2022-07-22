a114a254cdb77d116ed8f2c52fe525d9bb2b499b
Olasunkanmiidris15@gmail.com <Olasunkanmiidris15@gmail.com>
2022-07-23 00:30:36 +0100

    sucessfully finished the projectoject, front and back working perfectly

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
   auth0: {
-    url: "cofee-shop-app.us.auth0.com", // the auth0 domain prefix
+    url: "cofee-shop-app.us", // the auth0 domain prefix
     audience: "CoffeeShop", // the audience set for the auth0 app
     clientId: "60QMpu6DB066ZL51KR52YC1CZmu92uwk", // the client id generated for the auth0 app
-    callbackURL: "http://localhost:8100", // the base url of the running ionic application.
+    callbackURL: "https://localhost:8100", // the base url of the running ionic application.
   },
 };
